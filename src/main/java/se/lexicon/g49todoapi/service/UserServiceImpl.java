a67673287d54239739189115afce4aa55f43c42e
Camelia Ciuca <camelia1414@gmail.com>
2024-06-11 15:34:14 +0200

    UserService Impl

--- src/main/java/se/lexicon/g49todoapi/service/UserServiceImpl.java
+++ src/main/java/se/lexicon/g49todoapi/service/UserServiceImpl.java
@@ -1,7 +1,30 @@
 package se.lexicon.g49todoapi.service;
 
 import org.springframework.stereotype.Service;
+import se.lexicon.g49todoapi.domanin.dto.UserDTOForm;
+import se.lexicon.g49todoapi.domanin.dto.UserDTOView;
 
 @Service
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
+
+
+    @Override
+    public UserDTOView register(UserDTOForm dtoForm) {
+        return null;
+    }
+
+    @Override
+    public UserDTOView getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public void disableEmail(String email) {
+
+    }
+
+    @Override
+    public void enableEmail(String email) {
+
+    }
 }
