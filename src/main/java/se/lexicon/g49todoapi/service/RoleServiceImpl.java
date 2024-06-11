35475c28e8878d5594d9c2354adbd8d3911923d7
Camelia Ciuca <camelia1414@gmail.com>
2024-06-11 15:09:27 +0200

    RoleServiceImpl

--- src/main/java/se/lexicon/g49todoapi/service/RoleServiceImpl.java
+++ src/main/java/se/lexicon/g49todoapi/service/RoleServiceImpl.java
@@ -2,23 +2,37 @@ package se.lexicon.g49todoapi.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import se.lexicon.g49todoapi.converter.RoleConverter;
 import se.lexicon.g49todoapi.domanin.dto.RoleDTOView;
 import se.lexicon.g49todoapi.domanin.entity.Role;
 import se.lexicon.g49todoapi.repository.RoleRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class RoleServiceImpl implements RoleService {
 
     private final RoleRepository roleRepository;
+    private final RoleConverter roleConverter;
+
     @Autowired
-    public RoleServiceImpl(RoleRepository roleRepository) {
+    public RoleServiceImpl(RoleRepository roleRepository, RoleConverter roleConverter) {
         this.roleRepository = roleRepository;
+        this.roleConverter = roleConverter;
     }
+
+
+
     @Override
     public List<RoleDTOView> getAll() {
         List <Role> roles =roleRepository.findAll();
-        return List.of();
+        List<RoleDTOView> roleDTOViewsList = new ArrayList<>();
+
+        for (Role entity : roles) {
+
+            roleDTOViewsList.add(roleConverter.toRoleDTO(entity));
+        }
+        return roleDTOViewsList;
     }
 }
