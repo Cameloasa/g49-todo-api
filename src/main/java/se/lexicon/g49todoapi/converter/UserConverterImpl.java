befd43331fef76dfd4a9a442953a0a2db939bbb0
Camelia Ciuca <camelia1414@gmail.com>
2024-06-12 15:25:32 +0200

    Repository package, Person & Task

--- src/main/java/se/lexicon/g49todoapi/converter/UserConverterImpl.java
+++ src/main/java/se/lexicon/g49todoapi/converter/UserConverterImpl.java
@@ -8,12 +8,16 @@ import se.lexicon.g49todoapi.domanin.entity.User;
 public class UserConverterImpl implements UserConverter {
     @Override
     public UserDTOView toUserDTO(User entity) {
-       UserDTOView dto = new UserDTOView(entity.getEmail(), entity.getRoles());
-       return dto;
+        return UserDTOView.builder().build();
+       //UserDTOView dto = new UserDTOView(entity.getEmail(), entity.getRoles());
+       //return dto;
     }
 
     @Override
     public User toUserEntity(UserDTOView dto) {
-        return new User(dto.getEmail());
+        return User.builder()
+                .email(dto.getEmail())
+                .build();
+        //return new User(dto.getEmail());
     }
 }
