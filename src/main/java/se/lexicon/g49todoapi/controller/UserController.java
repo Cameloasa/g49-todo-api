8d3c07790d5963bacd9073a374dd14d77c887dc3
Camelia Ciuca <camelia1414@gmail.com>
2024-06-20 13:56:42 +0200

    Role&User DTO form with validation,User Controller with Validation

--- src/main/java/se/lexicon/g49todoapi/controller/UserController.java
+++ src/main/java/se/lexicon/g49todoapi/controller/UserController.java
@@ -1,8 +1,14 @@
 package se.lexicon.g49todoapi.controller;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import se.lexicon.g49todoapi.domanin.dto.UserDTOForm;
 import se.lexicon.g49todoapi.domanin.dto.UserDTOView;
@@ -10,6 +16,7 @@ import se.lexicon.g49todoapi.service.UserService;
 
 @RequestMapping("api/v1/users")
 @RestController
+@Validated
 public class UserController {
 
     private final UserService userService;
@@ -20,14 +27,20 @@ public class UserController {
     }
 
     @PostMapping
-    public ResponseEntity<UserDTOView>doRegister(@RequestBody UserDTOForm dtoForm) {
+    public ResponseEntity<UserDTOView>doRegister(@RequestBody @Valid UserDTOForm dtoForm) {
         System.out.println("DTO Form: " + dtoForm);
         UserDTOView responseBody = userService.register(dtoForm);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseBody);
     }
 
     @GetMapping
-    public ResponseEntity<UserDTOView> doUserByEmail(@RequestParam String email) {
+    public ResponseEntity<UserDTOView> doUserByEmail(
+            @RequestParam
+            @NotEmpty
+            @NotNull
+            @Email
+            //@Pattern(regexp = "^[A-Za-z0-9+_.-]+@(.+)$", message = "Invalid email format") //simple email format
+            String email) {
         System.out.println(">>>>>>> getUserByEmail has been executed");
         System.out.println("Email: " + email);
 
