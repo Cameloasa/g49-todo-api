78e0da684beb5cc9f9c47251bd4e7874f6ead00b
Camelia Ciuca <camelia1414@gmail.com>
2024-06-20 14:34:17 +0200

    ErrorDTO

---
+++ src/main/java/se/lexicon/g49todoapi/exception/RestExceptionHandler.java
@@ -0,0 +1,20 @@
+package se.lexicon.g49todoapi.exception;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class RestExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        //Create an error DTO object
+
+        return super.handleMethodArgumentNotValid(ex, headers, status, request);
+    }
+}
